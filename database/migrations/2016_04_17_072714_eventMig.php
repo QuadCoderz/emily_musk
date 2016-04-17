60be2b45e4cc1cb933e726a143e6431ebafe1449
Mohammed Hassan Mourad <mohammed.mourad94@eng-st.cu.edu.eg>
2016-04-17 19:20:10 +0200

    first commit

--- database/migrations/2016_04_17_072714_eventMig.php
+++ database/migrations/2016_04_17_072714_eventMig.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class EventMig extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('event', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('phone_number');
-            $table->rememberToken();
+            $table->string('title');
+            //event_description
+            $table->date('starting_date');
+            $table->date('ending_date');
+            $table->time('starting_time');
+            $table->time('ending_time');
+            $table->float('fees');
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('event');
     }
 }
--- database/migrations/2016_04_17_073925_items_mig.php
+++ database/migrations/2016_04_17_073925_items_mig.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class ItemsMig extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('items', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('phone_number');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description');
+            $table->string('image_path');
+            $table->float('price');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('items');
     }
 }
